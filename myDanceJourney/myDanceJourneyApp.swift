8f048e2f93f21d68289d27c3c9c9bf5ccc8e32f8
rohan-grafiniti <rohan@grafiniti.com>
2023-07-11 16:19:39 -0700

    Initial Commit

---
+++ myDanceJourney/myDanceJourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  myDanceJourneyApp.swift
+//  myDanceJourney
+//
+//  Created by Rohan Kumar on 7/11/23.
+//
+
+import SwiftUI
+
+@main
+struct myDanceJourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
