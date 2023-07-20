6f839cae287286214c95867c66ec571c226e6480
Akanksha <akkanksha.kumar@gmail.com>
2023-07-20 21:54:10 +0400

    Welcome/Get started page and Quiz question #1 UI (back and forth between pages working)

--- myDanceJourney/Views/WelcomeView.swift
+++ myDanceJourney/Views/WelcomeView.swift
@@ -4,27 +4,134 @@
 //
 //  Created by Akanksha Kumar on 7/19/23.
 //
-
 import SwiftUI
 
 struct WelcomeView: View {
+    @State private var isQuestionViewPresented = false
+
     var body: some View {
         NavigationView {
             VStack {
-                Text("Welcome!") // Title
+                Text("Welcome!")
                     .font(.largeTitle)
                     .fontWeight(.bold)
                     .padding(.bottom, 20)
 
-                Text("Let's get started...") // Subtitle
+                Text("Let's get to know you...")
                     .font(.headline)
                     .foregroundColor(.gray)
                     .padding(.bottom, 40)
+
+                NavigationLink(
+                    destination: QuestionView(question: questionLevel),
+                    isActive: $isQuestionViewPresented,
+                    label: {
+                        Text("Get Started")
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(Color.blue)
+                            .cornerRadius(15)
+                    }
+                )
+            }
             .padding()
         }
     }
+}
+
+// ---------------------------------------
+
+struct Option {
+    var mainText: String
+    var subtitle: String
+}
+
+struct Question {
+    var text: String
+    var options: [Option]
+}
+
+// ----------------------------------------
+
+struct QuestionView: View {
+    var question: Question
+
+    var body: some View {
+        VStack {
+            Text(question.text)
+                .font(.title)
+                .fontWeight(.bold)
+                .padding(.bottom, 20)
+//            Text(question.text)
+//                .font(.headline)
+//                .padding(.bottom, 20)
+
+            ForEach(question.options, id: \.mainText) { option in
+                OptionButton(option: option)
+                    .padding(.bottom, 10)
+            }
+        }
+        .padding()
     }
 }
+
+struct OptionButton: View {
+    var option: Option
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(option.mainText)
+                .font(.headline)
+            Text(option.subtitle)
+                .font(.subheadline)
+                .foregroundColor(.gray)
+        }
+        .padding()
+        .background(Color.white)
+        .cornerRadius(10)
+        .shadow(radius: 4)
+    }
+}
+
+// --------------------------------------------
+
+let questionLevel = Question(
+    text: "What level of dance are you at?",
+    options: [
+        Option(mainText: "Brand new", subtitle: "I can't dance, but want to learn!"),
+        Option(mainText: "Beginner", subtitle: "I can follow a beat.                "),
+        Option(mainText: "Intermediate", subtitle: "I'm comfortable learning routines."),
+        Option(mainText: "Advanced", subtitle: "I'm a trained dancer.            ")
+    ]
+)
+
+let questionGoal = Question(
+    text: "What is your main dance goal?",
+    options: [
+        Option(mainText: "Dance in social settings", subtitle: "eg. parties, weddings, clubs"),
+        Option(mainText: "Learn cool dance moves I see on social media", subtitle: ""),
+        Option(mainText: "Learn a new dance style", subtitle: "eg. Jazz, Hip-Hop, Bollywood"),
+        Option(mainText: "Get a workout and have some fun!", subtitle: "")
+    ]
+)
+
+let questionStyle = Question(
+    text: "What is your favorite dance style?",
+    options: [
+        Option(mainText: "Ballet", subtitle: ""),
+        Option(mainText: "Ballroom", subtitle: ""),
+        Option(mainText: "Contemporary", subtitle: ""),
+        Option(mainText: "Folk/Traditional", subtitle: ""),
+        Option(mainText: "Indian Classical", subtitle: ""),
+        Option(mainText: "Jazz", subtitle: ""),
+        Option(mainText: "Latin", subtitle: ""),
+        Option(mainText: "Street/Urban", subtitle: ""),
+        Option(mainText: "Tap", subtitle: ""),
+        Option(mainText: "Other", subtitle: "")
+    ]
+)
+
+// -----------------------------------------------
 
 struct WelcomeView_Previews: PreviewProvider {
     static var previews: some View {
